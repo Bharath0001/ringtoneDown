5fbf9a955aa2dbb7edadbf67b7c5390fdaac4dec
Bharath <bharathvijay2002@gmail.com>
2025-02-17 20:39:18 +0530

    small change

--- frontend/src/pages/MovieRingtonePage.js
+++ frontend/src/pages/MovieRingtonePage.js
@@ -21,9 +21,9 @@ function MovieRingtonePage() {
        
     return (
         <>
-            <div className="min-h-screen text-white">
+            <div className="min-h-screen text-white ">
                 <SearchBar />
-                <div className="max-w-6xl mx-auto pt-24 px-4">
+                <div className="max-w-6xl mx-auto pt-24 px-4 py-4">
                     <h2 className="text-2xl font-bold mb-4 ">{audioCard.length > 0 ? `${audioCard[0]?.movie?.title ?? "Loading..."}`: "No ringtones for this movie" } Ringtones</h2>
                     <div className="grid grid-cols md:grid-cols-3 lg:grid-cols-3 gap-4">
                         {audioCard.map((ringtone)=>
